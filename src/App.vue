6daa02cd80a9559d7b342adc61201f50b6801bce
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-02 09:34:15 +0200

    Scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
+    <Header/>
+    <Characters>
+      <Character/>
+    </Characters>
+    
   </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
+import Characters from './components/Characters.vue'
+import Character from './components/Character.vue'
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Characters,
+    Character
   }
 }
 </script>
