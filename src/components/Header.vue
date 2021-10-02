8212bbe20c09c3870d3e51f3e0b68061b2f50b70
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-02 10:17:38 +0200

    added header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,19 @@
 <template>
-    <header></header>
+    <header class="container py-5">
+        <h1 class="text-center">{{title}}</h1>
+    </header>
 </template>
 
 <script>
 export default {
     name : "Header",
+    props : ['title']
 }
 </script>
 
 <style lang="scss" scoped>
+    h1 {
+        font-weight: 700;
+    }
 
 </style>
