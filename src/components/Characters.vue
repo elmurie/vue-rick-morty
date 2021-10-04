7b9c4540324d6bce504956e9303a9a8fcb069853
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-04 12:52:19 +0200

    added searchbar

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,7 +1,10 @@
 <template>
     <section class="characters container">
+        <div class="row col-12">
+            <SearchBar @search="searchCharacters" />
+        </div>
         <div class="row">
-            <div class="col-6 col-md-4 col-lg-3 mb-3" v-for="(element, index) in characters" :key="index">
+            <div class="col-6 col-md-4 col-lg-3 mb-3" v-for="(element, index) in charactersFIltered" :key="index">
                 <Character :info="element"/>
             </div>
         </div>
@@ -12,14 +15,18 @@
 <script>
 import Character from './Character.vue';
 import axios from 'axios';
+import SearchBar from './SearchBar.vue';
+
 export default {
     name : "Characters",
     components : {
-        Character
+        Character,
+        SearchBar
     },
     data() {
         return {
-            characters : []
+            characters : [],
+            searchText : ''
         }
     },
     created() {
@@ -27,6 +34,21 @@ export default {
         .then( (response) => {
             this.characters = response.data;
         } );
+    },
+    methods: {
+        searchCharacters(text) {
+            this.searchText = text;
+        }
+    },
+    computed: {
+        charactersFIltered() {
+            const arrFiltered =  this.characters.filter(
+                (elm) => {
+                    return elm.name.toLowerCase().includes(this.searchText.toLowerCase());
+                }
+            );
+            return arrFiltered;
+        }
     }
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <form class="d-flex mb-5">
+        <input class="form-control" type="text" placeholder="Search Characters" v-model="searchText">
+        <button type="submit" class="btn btn-primary mx-3" @click.prevent="$emit('search', searchText)">Search</button>
+        <button class="btn btn-warning" @click.prevent="resetSearch()">Reset</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "SearchBar",
+    data() {
+        return {
+            searchText : ''
+        }
+    },
+    methods : {
+        resetSearch() {
+            this.searchText = '';
+            this.$emit('search', this.searchText);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
